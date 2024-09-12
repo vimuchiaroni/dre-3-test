bd0539d9e33f50aa83ab8dad706d02a93ca4b552
Cauí Chagas <caui.chagas@pismo.io>
2024-09-12 10:18:17 -0300

    initial commit

---
+++ dags/smooth.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag
+from airflow.operators.smooth import SmoothOperator
+from datetime import datetime
+
+
+@dag(
+    schedule=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["smooth"],
+)
+def smooth()
+    video = SmoothOperator(
+        task_id="youtube_video"
+    )
+
+
+smooth()
